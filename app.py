ec6f58053320c15c38935ba4a178551a99ca2426
jiahao.li <jiahao.li@marketin.cn>
2017-03-22 15:58:49 +0800

    配置跨域

--- app.py
+++ app.py
@@ -3,6 +3,21 @@
 from flask import Flask, jsonify, render_template
 import json
 from spider.barcode_spider import BarCodeSpider
+from functools import wraps
+from flask import make_response
+
+
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
+        return rst
+    return wrapper_fun
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -15,6 +30,7 @@ def index():
 
 
 @app.route('/api/good/<barcode>', methods=['GET'])
+@allow_cross_domain
 def get_good(barcode):
     good = BarCodeSpider.get_good(barcode)
     return jsonify(good.__dict__)
